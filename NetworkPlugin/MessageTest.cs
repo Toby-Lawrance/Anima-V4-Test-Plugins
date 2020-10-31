fdf980458ded4e5a467f03dcc20c5fd107930fd2
Toby <toby.lawrance@gmail.com>
2020-10-31 22:26:15 +0000

    Add projects

---
+++ NetworkPlugin/MessageTest.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using Core;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace NetworkPlugin
+{
+    public static class MessageHolder
+    {
+        private static bool IPv6Support = Socket.OSSupportsIPv6;
+
+        public class MessageServer : Core.Plugins.Module
+        {
+            private StreamReader inStream;
+            private TcpListener server;
+            private bool SuccessfulSetup = false;
+
+            public MessageServer() : base("MessageServer", "Receives messages from other servers", 1) { Enabled = false; }
+
+            public override void Init()
+            {
+                base.Init();
+                if (!IPv6Support)
+                {
+                    Anima.Instance.ErrorStream.WriteLine("IPv6 is not supported");
+                    return;
+                }
+
+                if (server is null)
+                {
+                    if (Anima.Instance.KnowledgePool.Exists("IP-Port"))
+                    {
+                        Anima.Instance.KnowledgePool.TryGetValue("IP-Port", out int Port);
+                        var info = Dns.GetHostEntry(Dns.GetHostName());
+                        info.AddressList.Select(a => Anima.Instance.WriteLine($"Server Address: {a}"));
+                        var address = info.AddressList[0];
+                        var end = new IPEndPoint(address, Port);
+                        server = new TcpListener(IPAddress.IPv6Any, Port);
+                        server.Start();
+                        this.StartTask(ListenAndRespond,TaskCreationOptions.LongRunning);
+                        SuccessfulSetup = true;
+                    }
+                    else
+                    {
+                        Anima.Instance.KnowledgePool.TryInsertValue("IP-Addresses", new string[] { });
+                        Anima.Instance.KnowledgePool.TryInsertValue("IP-Port", 0);
+                        Anima.Instance.ErrorStream.WriteLine("Error: Needed to create values in Anima pool");
+                    }
+                }
+            }
+
+            private void ListenAndRespond()
+            {
+                while (true)
+                {
+                    try
+                    {
+                        var client = server.AcceptTcpClient();
+
+                        inStream = new StreamReader(client.GetStream());
+
+                        string ReadContents = "";
+                        string line = "";
+                        while ((line = inStream.ReadLine()) != "<EOF>")
+                        {
+                            ReadContents += line + Anima.NewLineChar;
+                        }
+                        Anima.Instance.WriteLine($"Received: {ReadContents} from: {client.Client.RemoteEndPoint}");
+                        Anima.Instance.MailBoxes.PostMessage(new Message(client.Client.RemoteEndPoint.ToString(), this.Identifier,
+                            "Remote", ReadContents));
+                        client.Close();
+                    }
+                    catch(SocketException se)
+                    {
+                        if (se.SocketErrorCode == SocketError.AddressFamilyNotSupported)
+                        {
+                            Anima.Instance.ErrorStream.WriteLine(se.Message);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Anima.Instance.ErrorStream.WriteLine(e.Message);
+                    }
+                }
+            }
+
+            public override void Tick()
+            {
+                if (!SuccessfulSetup) return;
+            }
+
+            public override void Close()
+            {
+                base.Close();
+                if (!SuccessfulSetup) return;
+                server.Stop();
+            }
+        }
+
+        public class MessageClient : Core.Plugins.Module
+        {
+            private Dictionary<IPAddress,int> outBoundClients;
+            private int port;
+            private bool SuccessfulSetup = false;
+            private ManualResetEvent GetHostEntryFinished = new ManualResetEvent(false);
+
+            private static readonly int MaxFailures = 10;
+
+            public MessageClient() : base("MessageClient", "Sends messages to other computers", 2) { Enabled = false; }
+
+            private async Task<(bool,IPAddress)> TryConnect(IPAddress a, int p)
+            {
+                try
+                {
+                    var tcp = new TcpClient(AddressFamily.InterNetworkV6);
+                    await tcp.ConnectAsync(a,p);
+                    return (true,a);
+                }
+                catch (Exception e)
+                {
+                    Anima.Instance.ErrorStream.WriteLine($"Unable to connect to:{a} because {e.Message}");
+                    return (false,a);
+                }
+            }
+
+            private TcpClient TryConnectClient(IPAddress a, int p)
+            {
+                try
+                {
+                    var tcp = new TcpClient(AddressFamily.InterNetworkV6);
+                    tcp.Connect(a, p);
+                    return tcp;
+                }
+                catch (Exception e)
+                {
+                    Anima.Instance.ErrorStream.WriteLine($"Unable to connect to:{a} because {e.Message}");
+                    return null;
+                }
+            }
+
+            public override void Init()
+            {
+                base.Init();
+                if (!IPv6Support)
+                {
+                    Anima.Instance.ErrorStream.WriteLine("IPv6 is not supported");
+                    return;
+                }
+
+                if (outBoundClients is not null) return;
+
+                outBoundClients = new Dictionary<IPAddress, int>();
+                if (Anima.Instance.KnowledgePool.Exists("IP-Addresses") && Anima.Instance.KnowledgePool.Exists("IP-Port"))
+                {
+                    Anima.Instance.KnowledgePool.TryGetValue("IP-Addresses", out IEnumerable<string> addresses);
+                    Anima.Instance.KnowledgePool.TryGetValue("IP-Port", out port);
+
+                    var connectionTasks = addresses.Select(IPAddress.Parse).Select(ip => TryConnect(ip,port)).ToArray();
+
+                    Task.WaitAll(connectionTasks);
+
+                    foreach (var task in connectionTasks)
+                    {
+                        if (task.Result.Item1)
+                        {
+                            outBoundClients.Add(task.Result.Item2,1);
+                        }
+                        else
+                        {
+                            outBoundClients.Add(task.Result.Item2,0);
+                        }
+                    }
+                    SuccessfulSetup = true;
+                }
+                else
+                {
+                    Anima.Instance.KnowledgePool.TryInsertValue("IP-Addresses", new string[] { });
+                    Anima.Instance.KnowledgePool.TryInsertValue("IP-Port", 0);
+                    Anima.Instance.ErrorStream.WriteLine("Error: Needed to create IP-Addresses in Anima pool");
+                }
+            }
+
+            private async Task<(bool,IPAddress)> TrySendMessage(IPAddress a, string message)
+            {
+                var tcp = TryConnectClient(a, port);
+                if (tcp is null) return (false,a);
+
+                var strem = new StreamWriter(tcp.GetStream());
+                await strem.WriteLineAsync(message);
+                await strem.FlushAsync();
+                return (true,a);
+
+            }
+
+            public override void Tick()
+            {
+                if (!SuccessfulSetup) return;
+                if (!Anima.Instance.KnowledgePool.Exists("Count")) return;
+
+                Anima.Instance.WriteLine($"Attempting to send network messages");
+
+                var message = Anima.Serialize(new KeyValuePair<string, KeyValuePair<Type, object>>("Count", Anima.Instance.KnowledgePool.Pool["Count"]));
+                var tasks = outBoundClients.Where(tup => tup.Value > (MaxFailures * -1)).Select(tup => TrySendMessage(tup.Key,message)).ToArray();
+                Task.WaitAll(tasks);
+                foreach (var task in tasks)
+                {
+                    if (task.Result.Item1)
+                    {
+                        outBoundClients[task.Result.Item2]++;
+                    }
+                    else
+                    {
+                        outBoundClients[task.Result.Item2]--;
+                    }
+                }
+            }
+
+            public override void Close()
+            {
+                base.Close();
+                if (!SuccessfulSetup) return;
+            }
+        }
+    }
+
+}
---
+++ TestPlugin/TestPlugins.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core;
+using Core.Plugins;
+
+namespace TestPlugin
+{
+    public class TestModule : Module
+    {
+
+        public TestModule() : base("Test Plugin", "A test plugin",TimeSpan.FromSeconds(1)) {}
+
+        public override void Init()
+        {
+            base.Init();
+            if (Anima.Instance.KnowledgePool.Exists("Count")) return;
+            var succ = Anima.Instance.KnowledgePool.TryInsertValue("Count", 0);
+            if(!succ) {Anima.Instance.ErrorStream.WriteLine("Unable to set count");}
+
+        }
+
+        public override void Tick()
+        {
+            var succ = Core.Anima.Instance.KnowledgePool.TryGetValue("Count",out int Count);
+            if (succ)
+            {
+                Anima.Instance.WriteLine($"Count:{Count}");
+                var setSucc = Anima.Instance.KnowledgePool.TrySetValue("Count", Count + 1);
+                if(setSucc == false) {Anima.Instance.WriteLine("Unable to increment Count");}
+            }
+            else
+            {
+                Anima.Instance.ErrorStream.WriteLine("Couldn't get Count");
+            }
+        }
+    }
+
+
+    public class TestModule2 : Module
+    {
+        public TestModule2() : base("Test Plugin 2", "Testing if multiple modules can be loaded from a single dll",TimeSpan.FromSeconds(3)) {}
+
+        public override void Tick()
+        {
+            if (Core.Anima.Instance.MailBoxes.CheckNumMessages(this) == 0)
+            {
+                Core.Anima.Instance.WriteLine($"No messages for: {this.Identifier}");
+            }
+            while (Core.Anima.Instance.MailBoxes.CheckNumMessages(this) > 0)
+            {
+                Core.Anima.Instance.WriteLine(Core.Anima.Instance.MailBoxes.GetMessage(this));
+            }
+            
+        }
+    }
+
+    public class TestModule3 : Module
+    {
+        public TestModule3() : base("Test Plugin 3", "Test for embedded", TimeSpan.FromSeconds(1)) { Enabled = true; }
+
+        public override void Init()
+        {
+            base.Init();
+            Anima.Instance.KnowledgePool.TryInsertValue("Count", 0);
+        }
+
+        public override void Tick()
+        {
+            var succ = Anima.Instance.KnowledgePool.TryGetValue("Count", out int Count);
+            if(!succ) {return;}
+
+            var NetMsg = new Core.Network.NetMessage(Environment.MachineName,this.Identifier,"Test Plugin 2", string.Concat(Enumerable.Repeat("a", Count)));
+            var Msg = Message.CreateMessage(this, "Relay-Client", Anima.Serialize(NetMsg));
+            Anima.Instance.MailBoxes.PostMessage(Msg);
+        }
+    }
+}
