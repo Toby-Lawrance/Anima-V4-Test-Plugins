7170f85af21472dadad3fd3ff8dcf77f0c4b1160
Toby <toby.lawrance@gmail.com>
2021-05-22 16:10:49 +0100

    New style

--- TestPlugin/TestPlugins.cs
+++ TestPlugin/TestPlugins.cs
@@ -2,30 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using Core;
+using Core.CoreData;
 using Core.Plugins;
 
 namespace TestPlugin
 {
     public class TestModule : Module
     {
+        private KnowledgeBase<int> counting = new KnowledgeBase<int>();
 
         public TestModule() : base("Test Plugin", "A test plugin",TimeSpan.FromSeconds(1)) {}
 
         public override void Init()
         {
             base.Init();
-            var succ = Anima.Instance.KnowledgePool.TryInsertValue("Count", 0);
+            var succ = counting.TryInsertValue("Count", 0);
             if(succ) {Anima.Instance.WriteLine("Set count initial value");}
 
         }
 
         public override void Tick()
         {
-            var succ = Core.Anima.Instance.KnowledgePool.TryGetValue("Count",out int Count);
+            var succ = counting.TryGetValue("Count",out int Count);
             if (succ)
             {
+                var msg = Message<int>.CreateMessage(this, "Test Plugin 2", Count);
+                Anima.Instance.MailBoxes.PostMessage(msg);
+                
                 Anima.Instance.WriteLine($"Count:{Count}");
-                var setSucc = Anima.Instance.KnowledgePool.TrySetValue("Count", Count + 1);
+                var setSucc = counting.TrySetValue("Count", Count + 1);
                 if(setSucc == false) {Anima.Instance.WriteLine("Unable to increment Count");}
             }
             else
@@ -38,7 +43,7 @@ namespace TestPlugin
 
     public class TestModule2 : Module
     {
-        public TestModule2() : base("Test Plugin 2", "Testing if multiple modules can be loaded from a single dll",TimeSpan.FromSeconds(3)) {}
+        public TestModule2() : base("Test Plugin 2", "Testing if multiple modules can be loaded from a single dll",TimeSpan.FromSeconds(5)) {}
 
         public override void Tick()
         {
@@ -48,30 +53,9 @@ namespace TestPlugin
             }
             while (Core.Anima.Instance.MailBoxes.CheckNumMessages(this) > 0)
             {
-                Core.Anima.Instance.WriteLine(Core.Anima.Instance.MailBoxes.GetMessage(this));
+                Core.Anima.Instance.WriteLine(Core.Anima.Instance.MailBoxes.GetMessage<int>(this));
             }
             
-        }
-    }
-
-    public class TestModule3 : Module
-    {
-        public TestModule3() : base("Test Plugin 3", "Test for embedded", TimeSpan.FromSeconds(1)) { Enabled = true; }
-
-        public override void Init()
-        {
-            base.Init();
-            Anima.Instance.KnowledgePool.TryInsertValue("Count", 0);
-        }
-
-        public override void Tick()
-        {
-            var succ = Anima.Instance.KnowledgePool.TryGetValue("Count", out int Count);
-            if(!succ) {return;}
-
-            var NetMsg = new Core.Network.NetMessage(Environment.MachineName,this.Identifier,"Test Plugin 2", string.Concat(Enumerable.Repeat("a", Count)));
-            var Msg = Message.CreateMessage(this, "Relay-Client", Anima.Serialize(NetMsg));
-            Anima.Instance.MailBoxes.PostMessage(Msg);
         }
     }
 }
